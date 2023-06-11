ba7540723804a475256a7f44807265beaf85373d
bherth <bherthchavez@gmail.com>
2023-06-11 18:29:00 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
       <div className="h-screen bg-slate-950">
-        <div className="grid place-items-center pt-20 gap-4">
-          <h2 className="text-gray-200 text-3xl font-bold"> Fetching User List Using Redux </h2>
+        <div className="grid place-items-center pt-20 gap-4 text-center">
+          <h2 className="text-gray-200 text-2xl md:text-3xl font-bold"> Fetching User List Using Redux </h2>
           <div className="px-10 container max-w-4xl mx-auto">
             <UserList />
           </div>
